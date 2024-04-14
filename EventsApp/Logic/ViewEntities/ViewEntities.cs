0927d827fb4203f9e5d23b82e017d95003b6e7fd
Daniel Sasu <danielvlads@gmail.com>
2024-04-14 13:18:45 +0300

    Details Page

---
+++ EventsApp/Logic/ViewEntities/ViewEntities.cs
@@ -0,0 +1,44 @@
+﻿using EventsApp.Logic.Entities;
+using EventsApp.Logic.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsApp.Logic.ViewEntities
+{
+    public class EventCard
+    {
+        public Guid EventGUID { get; set; }
+        public string OrganizerName { get; set; }
+        public string Title { get; set; }
+        public string Categories { get; set; }
+        public string Location { get; set; }
+        public int MaxParticipants { get; set; }
+        public string Description { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string BannerURL { get; set; }
+        public string LogoURL { get; set; }
+        public int AgeLimit { get; set; }
+        public float EntryFee { get; set; }
+
+        public EventCard(EventInfo evt)
+        {
+            EventGUID = evt.GUID;
+            OrganizerName = UsersManager.GetUser(evt.organizerGUID).name;
+            Title = evt.eventName;
+            Categories = evt.categories;
+            Location = evt.location;
+            MaxParticipants = evt.maxParticipants;
+            Description = evt.description;
+            StartDate = evt.startDate.ToString();
+            EndDate = evt.endDate.ToString();
+            BannerURL = evt.bannerURL;
+            LogoURL = evt.logoURL;
+            AgeLimit = evt.ageLimit;
+            EntryFee = evt.entryFee;
+        }
+    }
+}
--- EventsApp/MainPage.xaml.cs
+++ EventsApp/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using EventsApp.Logic.Entities;
 using EventsApp.Logic.Extensions;
 using EventsApp.Logic.Managers;
+using EventsApp.Logic.ViewEntities;
 
 namespace EventsApp
 {
@@ -44,38 +45,6 @@ namespace EventsApp
             return eventCards;
         }
 
-        public class EventCard
-        {
-            public Guid EventGUID { get; set; }
-            public string OrganizerName { get; set; }
-            public string Title { get; set; }
-            public string Categories { get; set; }
-            public string Location { get; set; }
-            public int MaxParticipants { get; set; }
-            public string Description { get; set; }
-            public string StartDate { get; set; }
-            public string EndDate { get; set; }
-            public string BannerURL { get; set; }
-            public string LogoURL { get; set; }
-            public int AgeLimit { get; set; }
-            public float EntryFee { get; set; }
-            
-            public EventCard(EventInfo evt)
-            {
-                EventGUID = evt.GUID;
-                OrganizerName = UsersManager.GetUser(evt.organizerGUID).name;
-                Title = evt.eventName;
-                Categories = evt.categories;
-                Location = evt.location;
-                MaxParticipants = evt.maxParticipants;
-                Description = evt.description;
-                StartDate = evt.startDate.ToString();
-                EndDate = evt.endDate.ToString();
-                BannerURL = evt.bannerURL;
-                LogoURL = evt.logoURL;
-                AgeLimit = evt.ageLimit;
-                EntryFee = evt.entryFee;
-            }
-        }
+        
     }
 }
