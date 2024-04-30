aaff0c875f2756112cb53207ff875c864806a3eb
wasy2023 <tk60726@gmail.com>
2024-04-30 17:28:50 +0300

    baby steps

--- EventsAppTests_XUnitTest/ExtensionsTest_StructExtensions.cs
+++ EventsAppTests_XUnitTest/ExtensionsTest_StructExtensions.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace EventsAppTests_XUnitTest
 {
-    internal class ExtensionsTest_StructExtensions
+    public class ExtensionsTest_StructExtensions
     {
+
+        public struct TestStruct
+        { 
+            [PrimaryKey]
+            public int Id { get; set; }
+
+            [Table("TestTable")]
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void GetIdentifier_ReturnsCorrectIdentifier()
+        {
+            var testObj = new TestStruct { Id = 1, Name = "Test" };
+            var expectedIdentifier = new Identifier(new Dictionary<string, object> { { "Id", 1 } });
+
+            var identifier = testObj.GetIdentifier();
+
+            Assert.Equal(expectedIdentifier, identifier);
+        }
+
+        [Fact]
+        public void GetTableName_ReturnsCorrectTableName()
+        {
+            var testObj = new TestStruct();
+
+            var tableName = testObj.GetTableName();
+
+            Assert.Equal("TestTable", tableName);
+        }
     }
 }
