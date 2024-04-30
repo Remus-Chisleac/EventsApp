fd275ff4c68eb37f37ba11aa5cc78609c5d11a16
wasy2023 <tk60726@gmail.com>
2024-04-30 17:26:17 +0300

    baby steps

---
+++ EventsAppTests_XUnitTest/ExtensionsTest_StructExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAppTests_XUnitTest
+{
+    internal class ExtensionsTest_StructExtensions
+    {
+    }
+}
